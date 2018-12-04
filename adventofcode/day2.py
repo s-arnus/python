28c9dbce800b5b2b9e189b4b8a8a02a746d507e9
s-arnus <28957746+s-arnus@users.noreply.github.com>
2018-12-04 21:17:55 +0200

    Added part 2 to the same script

--- adventofcode/day2.py
+++ adventofcode/day2.py
@@ -60,3 +60,64 @@ with open("input2.txt", "r") as file:
 print("Number of times two letters seen in a line: " + str(count_two))
 print("Number of times three letters seen in a line: " + str(count_three))
 print("Checksum: " + str(count_two * count_three))
+
+#PART 2
+#The boxes will have IDs which differ by exactly one character at the same position in both strings. For example, given the following box IDs:
+#abcde
+#fghij
+#klmno
+#pqrst
+#fguij
+#axcye
+#wvxyz
+#The IDs abcde and axcye are close, but they differ by two characters (the second and fourth). 
+#However, the IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+#
+#What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing character from either ID, producing fgij.)
+
+print('\n' + "PART TWO FALLOWS")
+#Empty list to store the input
+inputlist = []
+#Empty list to store the answer
+answer = []
+done = 0
+#For each line go throught all the other lines and compare it. Find a string that is different by exactly one character.
+with open("input2.txt", "r") as file:
+    line = file.readline()
+    while line:
+        #Add all the lines to a list that is later used over and over again
+        inputlist.append(line.strip())
+        line = file.readline()
+    #Go over the list of inputs to find a match
+    i = 0
+    while (i < len(inputlist)):
+        line1 = inputlist[i]
+        #print("Starting to compare: " + line1 + " index(i): " + str(i))
+        j = 0
+        #For every line check it agains other lines in the input list
+        while (j < len(inputlist)):
+            #Don't check the line with itself
+            if i is not j:
+                difference = 0
+                line2 = inputlist[j]
+                #print("Comparing against: " + line2 + " index(j): " + str(j))
+                #Check characters in different strings but same postions and add the number of differences to a variable
+                k = 0
+                while (k < len(line1)):
+                    #print("Comparing char: " + line1[k] + " against: " + line2[k])
+                    if line1[k] is not line2[k]:
+                        difference = difference + 1
+                    k = k + 1
+                #If there was only one difference then we have a winner
+                if difference == 1 and done is not 1:
+                    print("Line1 " + str(line1) + " and line2 " + str(line2) + " differ by " + str(difference))
+                    #Find the answer by removing the different character
+                    l = 0
+                    while (l < len(line1)):
+                        if line1[l] is line2[l]:
+                            answer.append(line1[l])
+                        l = l + 1
+                    done = 1
+            j = j + 1
+        i = i + 1
+print("Answer is: " + ''.join(answer))
